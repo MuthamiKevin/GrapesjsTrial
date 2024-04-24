7ab4455074790dd2774f617f1fad07baaab2a07b
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-24 11:36:07 +0300

    updat e

--- trial/index.js
+++ trial/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  // Send a response to the client
-  res.send('<script>window.open("https://example.com", "_blank");</script>');
+app.get('/', function (req, res) {
+  res.send("Hello world!");
 });
 
 app.listen(port, () => {
