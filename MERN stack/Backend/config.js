1cb06f2576f30f16479833287d2fcbe0c457fd74
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-26 20:15:48 +0300

    update

--- MERN stack/Backend/config.js
+++ MERN stack/Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://muthamikevin80:39899677@Kn@cluster0.1b6ozu7.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0'
+export const mongoDBURL = 'mongodb+srv://muthamikevin80:<39899677@Kn>@cluster0.1b6ozu7.mongodb.net/'
