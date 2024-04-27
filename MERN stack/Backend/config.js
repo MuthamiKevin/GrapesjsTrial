ac70d2d5f57a782b2f662e48f71c514ff0e53df0
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-27 20:11:45 +0300

    update

--- MERN stack/Backend/config.js
+++ MERN stack/Backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://muthamikevin80:<39899677@Kn>@cluster0.1b6ozu7.mongodb.net/'
+export const mongoDBURL = 
+        'mongodb+srv://muthami:39899677@bookk-store.3ayulqc.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookk-store'
