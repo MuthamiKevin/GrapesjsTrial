80ad72d35cd0a63858fed95afe832354e32b554b
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-27 21:14:04 +0300

    update

---
+++ MERN stack/Backend/models/bookModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+export const Cat = mongoose.model('Cat', booKSchema);
