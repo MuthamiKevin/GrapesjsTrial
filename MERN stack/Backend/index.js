ab883dedb0cf8a97a748297b37103003776da2a9
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-28 21:47:19 +0300

    update

--- MERN stack/Backend/index.js
+++ MERN stack/Backend/index.js
@@ -10,6 +10,38 @@ app.get('/', (req, res) => {
 });
 
 
+app.post('/books', async  (req, res) => {
+    try{
+
+    }catch(error){
+    console.log(error.message);
+    res.status(500).send({message:error.message}); }
+});
+
+app.post('/books', async (req, res) => {
+    try{    
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publisherYear
+        ) {
+            return res.status(400).send({message: 'Send all the required fields: title, title, author, publishYear',});
+        } 
+        const  newBooks = {
+            title:request.body.title,
+            author: request.body.author,
+            publisherYear: request.body.publisherYear,
+
+        }; 
+        const book = await book.create(newBook);
+        return res.status(201).send(book);
+
+    }catch (error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
 
 mongoose
     .connect(mongoDBURL)
