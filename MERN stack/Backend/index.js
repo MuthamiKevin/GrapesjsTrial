46fc5c1271b9adb8ecca7f98d272d3baa15a28c3
Kevin Muthami <muthamikevin80@gmail.com>
2024-05-06 18:19:17 +0300

    add

--- MERN stack/Backend/index.js
+++ MERN stack/Backend/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// Bootstrap the App
+require('./src/bootstrap')(app, __dirname, express);
 
 // Serve GrapeJS files from node_modules
 app.use('/grapejs', express.static(path.join(__dirname, 'node_modules', 'grapejs')));
 
 // Define route for the homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html')); // Assuming your HTML template is in the "views" directory
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+function listen() {
+    var server = app.listen(app.get('port'), function() {
+        require('./src/init').init();
+        console.log('Magic happens on port ' + server.address().port);
+    });
+}
+listen();
