d7da10f5c8553951949fbacd0bed221385a6441c
Kevin Muthami <muthamikevin80@gmail.com>
2024-05-15 19:54:40 +0300

    addd

--- take/script.js
+++ take/script.js
@@ -1,6 +1,6 @@
 const editor = grapesjs.init({
     // Indicate where to init the editor. You can also pass an HTMLElement
-    container: '#container',
+    container: '#gjs',
     // Get the content for the canvas directly from the element
     // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
     fromElement: true,
@@ -54,3 +54,10 @@ const editor = grapesjs.init({
       }
     ],
   });
+  const previewBtn = document.getElementById('previewBtn');
+const previewContainer = document.getElementById('previewContainer');
+
+previewBtn.addEventListener('click', () => {
+    const htmlContent = editor.getHtml(); // Get the HTML content from the editor
+    previewContainer.innerHTML = htmlContent; // Display the HTML content in the preview container
+});
